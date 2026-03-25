f34e0a97a4ca1969e74d287f6c5b25d9bf1e0f75
Federico Mione <federico.mione@campus.tu-berlin.de>
2026-03-25 16:14:35 -0300

    Import experiment form (stage1)

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -7,9 +7,10 @@ from apps.home import blueprint
 from flask import render_template, request
 from flask_login import login_required
 from jinja2 import TemplateNotFound
-from apps.home.forms import CreateExpConfigForm
+from apps.home.forms import ImportExperimentForm
 from apps.neomodel.model import User, Objective, Strain, Plasmid, Model, ComputationalMethod
 import yaml
+import os
 
 
 yaml_template = {""}
@@ -20,60 +21,84 @@ def index():
     return render_template('home/index.html', segment='index')
 
 
-@blueprint.route('/exp_config', methods=['GET', 'POST'])
+@blueprint.route('/import', methods=['GET', 'POST'])
 @login_required
-def create_experiment_config():
+def import_experiment():
+    form = ImportExperimentForm(request.form)
+    
+    if 'create' in form and form.validate_on_submit():
+        print("OKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK")
 
-    form = CreateExpConfigForm(request.form)
+        print(form.exp_file.name)
+        print(form.exp_file.data)
 
-    # --- buttons actions
-    # add responsible row
-    if form.add_responsible.data:
-        form.responsibles.append_entry(None)
-    # delete responsible row
-    form.resp_deleted.data = False
-    for idx, resp in enumerate(form.responsibles.entries):
-        if resp.resp_delete.data:      
-            del form.responsibles.entries[idx]
-            form.resp_deleted.data = True
+        if form.exp_file.data:
+            print("FILEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
+            image_data = request.FILES[form.exp_file.name].read()
 
-    # add mbr group row
-    if form.add_mbr_group.data:
-        form.mbr_groups.append_entry(None)
-    # delete responsible row
-    form.mbr_group_deleted.data = False
-    for idx, mbrg in enumerate(form.mbr_groups.entries):
-        if mbrg.mbr_group_delete.data:      
-            del form.mbr_groups.entries[idx]
-            form.mbr_group_deleted.data = True
+            print(image_data)
+
+            # open(os.path.join(UPLOAD_PATH, form.image.data), 'w').write(image_data)
+
+        pass
+
+    return render_template('home/import.html', segment="import", form=form)
+
+
+# @blueprint.route('/exp_config', methods=['GET', 'POST'])
+# @login_required
+# def create_experiment_config():
+
+#     form = CreateExpConfigForm(request.form)
+
+#     # --- buttons actions
+#     # add responsible row
+#     if form.add_responsible.data:
+#         form.responsibles.append_entry(None)
+#     # delete responsible row
+#     form.resp_deleted.data = False
+#     for idx, resp in enumerate(form.responsibles.entries):
+#         if resp.resp_delete.data:      
+#             del form.responsibles.entries[idx]
+#             form.resp_deleted.data = True
+
+#     # add mbr group row
+#     if form.add_mbr_group.data:
+#         form.mbr_groups.append_entry(None)
+#     # delete responsible row
+#     form.mbr_group_deleted.data = False
+#     for idx, mbrg in enumerate(form.mbr_groups.entries):
+#         if mbrg.mbr_group_delete.data:      
+#             del form.mbr_groups.entries[idx]
+#             form.mbr_group_deleted.data = True
    
 
-    # --- get data for select inputs
-    # set input data for objective
-    form.objective.choices = [obj.name for obj in Objective.nodes.all()]
+#     # --- get data for select inputs
+#     # set input data for objective
+#     form.objective.choices = [obj.name for obj in Objective.nodes.all()]
 
-    # set input data for responsibles
-    user_list = User.nodes.all()
-    for resp in form.responsibles.entries:
-        resp.resp_name.choices = [user.username for user in user_list] 
-        resp.resp_role.choices = ["laboratory_supervisor", "laboratory_experimentation", "experimental_planner", "computational_algorithms", "workflow_definition"]
+#     # set input data for responsibles
+#     user_list = User.nodes.all()
+#     for resp in form.responsibles.entries:
+#         resp.resp_name.choices = [user.username for user in user_list] 
+#         resp.resp_role.choices = ["laboratory_supervisor", "laboratory_experimentation", "experimental_planner", "computational_algorithms", "workflow_definition"]
 
-    # set input data for strains, objectives, plasmids, comp methods
-    strain_list = Strain.nodes.all()
-    plasmid_list = Plasmid.nodes.all()
-    model_list = Model.nodes.all()
-    comp_method_list = ComputationalMethod.nodes.all()
-    for mbrg in form.mbr_groups.entries:
-        mbrg.comp_method.choices = [comp_method.name for comp_method in comp_method_list] 
-        mbrg.strain.choices = [strain.name for strain in strain_list]
-        mbrg.plasmid.choices = [plasmid.name for plasmid in plasmid_list]
-        mbrg.model.choices = [model.name for model in model_list]   
+#     # set input data for strains, objectives, plasmids, comp methods
+#     strain_list = Strain.nodes.all()
+#     plasmid_list = Plasmid.nodes.all()
+#     model_list = Model.nodes.all()
+#     comp_method_list = ComputationalMethod.nodes.all()
+#     for mbrg in form.mbr_groups.entries:
+#         mbrg.comp_method.choices = [comp_method.name for comp_method in comp_method_list] 
+#         mbrg.strain.choices = [strain.name for strain in strain_list]
+#         mbrg.plasmid.choices = [plasmid.name for plasmid in plasmid_list]
+#         mbrg.model.choices = [model.name for model in model_list]   
 
-    if form.validate_on_submit():
-        print("estaaaa validadooooooooo")
-        # TODO: create yaml y descargar
+#     if form.validate_on_submit():
+#         print("estaaaa validadooooooooo")
+#         # TODO: create yaml y descargar
 
-    return render_template('home/exp_config.html', segment="exp_config", form=form)
+#     return render_template('home/exp_config.html', segment="exp_config", form=form)
 
 
 # generic route template
